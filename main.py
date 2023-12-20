5bb1a2db9bcb9cb99b0733ccd03f32d82857d33c
RamiroCyber <ramiro.dr@gmail.com>
2023-12-20 16:41:04 -0300

    up

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from external.request import Request
 if __name__ == '__main__':
     request = Request()
     # request.getStatistics("190160")
-    request.get_match_id(10)
+
+    matches_without_stats = request.get_match_id(10)
+    for match_id in matches_without_stats:
+        request.getStatistics(match_id)
+
     # request.obter_times(71, 2015,2022)
     # request.obter_partida(71, 2015, 2022)
